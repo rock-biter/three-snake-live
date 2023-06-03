8102e3157c466471de3bcf6d23d8012057622002
rock-biter <74297920+rock-biter@users.noreply.github.com>
2023-06-03 18:01:53 +0200

    ADD - animation

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import './style.css'
 import * as THREE from 'three'
+import gsap from 'gsap'
 
 /**
  * Scene
@@ -44,63 +45,41 @@ document.body.appendChild(renderer.domElement)
  */
 camera.position.z = 4
 
-// mesh.position.x = 2
-// mesh.position.set(1, 0, -2)
-const pos2 = new THREE.Vector3(2, 2, 0)
+const vel = 0.5
 
-// mesh.position.copy(pos2)
-// mesh.position = pos2
-// mesh.position.x += 1
-// mesh.position.y += 0.5
-// mesh.position.add(new THREE.Vector3(1, 0.5, 0))
-const mesh2 = mesh.clone()
-const mesh3 = mesh.clone()
-
-// scene.add(mesh2, mesh3)
-mesh2.position.x = -2
-mesh3.position.x = 2
-
-// mesh2.scale.set(1.2, 1.2, 1.2)
-mesh2.scale.multiplyScalar(1.2)
-mesh.scale.y = 2
-mesh3.scale.set(0.5, 0.75, 3)
-
-const axesHelper = new THREE.AxesHelper(2)
-mesh.add(axesHelper.clone())
-mesh2.add(axesHelper.clone())
-mesh3.add(axesHelper.clone())
-
-mesh2.rotation.y = THREE.MathUtils.degToRad(45)
-mesh2.rotation.z = THREE.MathUtils.degToRad(-15)
-mesh2.rotation.order = 'ZXY'
-
-console.log(THREE.MathUtils.radToDeg(mesh2.rotation.y))
-
-mesh.quaternion.copy(new THREE.Quaternion().random())
-console.log(mesh.rotation)
-
-camera.position.y = 1
-camera.position.set(-2, 1, 2)
-
-const group = new THREE.Group()
-group.add(mesh2, mesh3)
-scene.add(group)
-
-group.position.set(0, 1, -1)
-group.scale.multiplyScalar(0.5)
-
-const v = new THREE.Vector3()
-mesh2.getWorldPosition(v)
-
-camera.lookAt(v)
+// let time = Date.now()
+const clock = new THREE.Clock()
+mesh.scale.multiplyScalar(0)
 
 /**
  * frame loop
  */
 function tic() {
+	// const currentTime = Date.now()
+	// const deltaTime = (currentTime - time) / 1000
+	const deltaTime = clock.getDelta()
+	const time = clock.getElapsedTime()
+
+	// time = currentTime
+	// console.log(time)
+
+	camera.position.y = Math.sin(time * 2)
+	camera.lookAt(mesh.position)
+
+	// console.log(deltaTime)
+	// mesh.rotation.x += vel * deltaTime
+	// mesh.rotation.y += vel * deltaTime
+
 	renderer.render(scene, camera)
 
 	requestAnimationFrame(tic)
 }
 
 requestAnimationFrame(tic)
+
+function pop() {
+	gsap.to(mesh.scale, { duration: 2, x: 1, y: 1, z: 1 })
+	gsap.to(mesh.rotation, { duration: 1, x: 3.14, y: 3.14 })
+}
+
+window.addEventListener('click', pop)
