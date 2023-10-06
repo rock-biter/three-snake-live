3d899b7b5adca74a1081b081e6176afb1c52ed4c
rock-biter <74297920+rock-biter@users.noreply.github.com>
2023-10-07 00:26:38 +0200

    ADD - segments on candy

--- src/Candy.js
+++ src/Candy.js
@@ -6,7 +6,7 @@ import {
 } from 'three'
 import Entity from './Entity'
 
-const GEOMETRY = new SphereGeometry(0.3, 10, 10)
+const GEOMETRY = new SphereGeometry(0.3, 20, 20)
 const MATERIAL = new MeshStandardMaterial({
 	color: 0x614bdd,
 })
